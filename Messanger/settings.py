8b4fd4fe1bfe78677993251cd8b660d76285427b
SanketDawange <sanketdawange6@gmail.com>
2023-02-06 22:10:52 +0530

    DEBUG DISABLED

--- Messanger/settings.py
+++ Messanger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ijpg8uefzp6=i&_-$-i0tca&aozw=ifbt29-0s8$ys&*(6=ng%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
